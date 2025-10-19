9e33d1b8b1fea1c358317b587948aa84b2126f45
Mels <mels.vagharshyandev@gmail.com>
2025-10-19 21:29:33 +0400

    current fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    express.json({ limit: '50mb' }), // Adjust as needed
+    express.urlencoded({ limit: '50mb', extended: true }),
+  );
+
   app.enableCors({
     origin: ['https://uc-shop-fe.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
