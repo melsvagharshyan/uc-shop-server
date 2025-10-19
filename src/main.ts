afbdab525eaf0c79073bfac0d95dec3c8c09f607
Mels <mels.vagharshyandev@gmail.com>
2025-10-19 22:07:38 +0400

    current fix

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(
-    express.json({ limit: '50mb' }), // Adjust as needed
-    express.urlencoded({ limit: '50mb', extended: true }),
-  );
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://uc-shop-fe.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS',
-    );
-    if (req.method === 'OPTIONS') return res.sendStatus(200);
-    next();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
