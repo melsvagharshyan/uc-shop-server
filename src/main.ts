0b4f62a35a96b5849a4a8767b30e03b2b7b26f29
Mels <mels.vagharshyandev@gmail.com>
2025-10-19 21:23:27 +0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://uc-shop-fe.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true, // if you use cookies or authorization headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
