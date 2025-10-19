2ee61e0b695c4c89d92ffa14c33250e543d26fab
Mels <mels.vagharshyandev@gmail.com>
2025-10-19 23:03:50 +0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://uc-shop-fe.vercel.app'],
+    origin: [
+      'https://uc-shop-fe.vercel.app',
+      'https://uc-shop-fe-git-master-melsvagharshyans-projects.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'], // Make sure headers you want are allowed
